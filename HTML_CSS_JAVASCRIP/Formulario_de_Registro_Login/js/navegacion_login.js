47bd7c27e41194f947adbdbfc0c5594fec58fc91
Aquiles <santtialbiazul25@gmail.com>
2024-05-11 04:59:22 -0300

    Cree la carpeta app, src, img, css,

--- HTML_CSS_JAVASCRIP/Formulario_de_Registro_Login/js/navegacion_login.js
+++
@@ -1,7 +0,0 @@
-
-const botonRedireccionarLogin = document.getElementById('botonRedireccionarRegistro');
-
-botonRedireccionarLogin.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = './index_registro.html';
-});
