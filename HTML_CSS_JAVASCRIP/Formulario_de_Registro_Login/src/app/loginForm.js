cba8a17eb48defd70d5cdd8c0765653fd59effa1
Aquiles <santtialbiazul25@gmail.com>
2024-05-11 06:36:00 -0300

    Guardo en constantes los datos capturados de login and register

--- HTML_CSS_JAVASCRIP/Formulario_de_Registro_Login/src/app/loginForm.js
+++ HTML_CSS_JAVASCRIP/Formulario_de_Registro_Login/src/app/loginForm.js
@@ -2,4 +2,9 @@ const loginForm = document.querySelector('#login-form');
 //console.log(loginForm);
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    //Guardo en constantes los datos capturados de login
+    const email = loginForm['login-email']
+    const password = loginForm['login-password']
+    //console.log(email, password);
 })
